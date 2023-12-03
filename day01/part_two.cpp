ee955581213077358d436e2108fde4efba91f4c3
Juan Pedro Martín <johnny@Juans-Mac-mini.local>
2023-12-03 20:17:40 +0100

    updated day 2 still wrong

--- day01/part_two.cpp
+++ day01/part_two.cpp
@@ -1,90 +1,77 @@
 #include <cctype>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <ostream>
 #include <string>
-#include <unordered_map>
-#include <utility> // Para std::pair
+#include <utility>
 
-// Mapa de palabras a dígitos
-std::unordered_map<std::string, int> word_to_digit = {
+std::map<std::string, int> word_to_digit = {
     {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
     {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
 
-// Función para identificar y priorizar palabras numéricas en la cadena
-std::vector<std::pair<size_t, std::string>>
-findNumericWords(const std::string &line) {
-  std::vector<std::pair<size_t, std::string>> positions;
-
-  for (const auto &pair : word_to_digit) {
-    size_t pos = line.find(pair.first);
-    while (pos != std::string::npos) {
-      positions.push_back({pos, pair.first});
-      pos = line.find(pair.first, pos + 1);
-    }
-  }
-
-  // Ordenar por posición y longitud de palabra
-  std::sort(positions.begin(), positions.end(),
-            [](const std::pair<size_t, std::string> &a,
-               const std::pair<size_t, std::string> &b) {
-              return a.first < b.first ||
-                     (a.first == b.first && a.second.size() > b.second.size());
-            });
-
-  return positions;
-}
-
-// Función para buscar dígitos y palabras en la línea
 std::pair<char, char> findDigits(const std::string &line) {
-  auto numericWords = findNumericWords(line);
-
   char firstDigit = '0', lastDigit = '0';
+  std::string result;
+  size_t pos = 0;
 
-  // Comprobar si el primer y último carácter son dígitos numéricos
-  if (!numericWords.empty()) {
-    // Si el primer carácter es un dígito, usarlo
-    if (isdigit(line.front())) {
-      firstDigit = line.front();
-    } else {
-      firstDigit = '0' + word_to_digit[numericWords.front().second];
+  // Replace words with digits and form the result string
+  while (pos < line.length()) {
+    bool isReplaced = false;
+    for (const auto &pair : word_to_digit) {
+      if (line.compare(pos, pair.first.length(), pair.first) == 0) {
+        result += std::to_string(pair.second);
+        pos += pair.first.length();
+        isReplaced = true;
+        break;
+      }
     }
-
-    // Si el último carácter es un dígito, usarlo
-    if (isdigit(line.back())) {
-      lastDigit = line.back();
-    } else {
-      lastDigit = '0' + word_to_digit[numericWords.back().second];
+    if (!isReplaced) {
+      result += line[pos++];
     }
   }
+
+  // Find the first digit
+  for (char c : result) {
+    if (std::isdigit(c)) {
+      firstDigit = c;
+      break;
+    }
+  }
+
+  // Find the last digit
+  for (auto it = result.rbegin(); it != result.rend(); ++it) {
+    if (std::isdigit(*it)) {
+      lastDigit = *it;
+      break;
+    }
+  }
+
+  std::cout << line << " --> " << "[" << firstDigit << "," << lastDigit << "]" << std::endl;
 
   return {firstDigit, lastDigit};
 }
 
 int main() {
   std::ifstream file("calibration_values.txt");
-  std::string line;
-  int sum = 0;
-
-  if (file.is_open()) {
-    int counter = 0;
-    while (getline(file, line)) {
-      auto [firstDigit, lastDigit] = findDigits(line);
-
-      if (firstDigit != 0 && lastDigit != 0) {
-      std::cout << "Pair number " << counter << " :" << firstDigit << ", " << lastDigit << std::endl;
-        int value = (firstDigit - '0') * 10 + (lastDigit - '0');
-        sum += value;
-      }
-
-      counter++;
-    }
-    file.close();
-  } else {
-    std::cout << "No se pudo abrir el archivo" << std::endl;
+  if (!file.is_open()) {
+    std::cerr << "Failed to open the input file" << std::endl;
     return 1;
   }
 
-  std::cout << "Suma total de valores de calibración: " << sum << std::endl;
+  std::string line;
+  int sum = 0;
+
+  while (getline(file, line)) {
+    auto [firstDigit, lastDigit] = findDigits(line);
+    if (firstDigit != '0' && lastDigit != '0') {
+      int value = (firstDigit - '0') * 10 + (lastDigit - '0');
+      sum += value;
+    }
+  }
+
+  std::cout << "Total sum: " << sum << std::endl;
   return 0;
 }
+
