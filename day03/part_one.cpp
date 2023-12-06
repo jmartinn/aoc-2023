727bf8b3d6ce459bf31df676448413dcc52d6111
Juan Pedro Martin <juamp_m@hotmail.com>
2023-12-06 21:19:24 +0100

    day 3 part one finished

--- day03/part_one.cpp
+++ day03/part_one.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 bool is_number(char c) { return c >= '0' && c <= '9'; }
 
 bool is_dot(char c) { return c == '.'; }
 
-const std::pair<int, int> dirs[] = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0},   {-1, 1}, {0, 1},  {1, 1}};
+const std::pair<int, int> dirs[] = { {-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0},   {-1, 1}, {0, 1},  {1, 1} };
 
+// Get character from a given position
 char get_char(const std::vector<std::string> &schema, int y, int x) {
 
   if (y < 0 || y >= schema.size() || x < 0 || x >= schema[y].size()) {
@@ -18,6 +20,7 @@ char get_char(const std::vector<std::string> &schema, int y, int x) {
   return schema[y][x];
 }
 
+// Get the whole number by checking adjacent digits in the x axis
 std::string get_full_number(const std::vector<std::string> &schema, int y, int x) {
   std::string number;
   int i = x;
@@ -35,7 +38,7 @@ std::string get_full_number(const std::vector<std::string> &schema, int y, int x
 }
 
 int main() {
-  std::ifstream file("input.test");
+  std::ifstream file("input.prod");
   std::vector<std::string> schema;
   std::string line;
   int sum = 0;
@@ -51,28 +54,28 @@ int main() {
 
   file.close();
 
-  std::set<int> processedStarts;
+  std::set<std::pair<int, int>> processed_starts;
   for (int y = 0; y < schema.size(); ++y) {
     for (int x = 0; x < schema[y].size(); ++x) {
       if (is_number(schema[y][x])) {
-        bool isAdjacentToSymbol = false;
+        bool is_adjacent_to_symbol = false;
         for (const auto &dir : dirs) {
-          char adjacentChar = get_char(schema, y + dir.first, x + dir.second);
-          if (!is_dot(adjacentChar) && !is_number(adjacentChar) &&
-              adjacentChar != '\0') {
-            isAdjacentToSymbol = true;
+          char adjacent_char = get_char(schema, y + dir.first, x + dir.second);
+          if (!is_dot(adjacent_char) && !is_number(adjacent_char) && adjacent_char != '\0') {
+            is_adjacent_to_symbol = true;
             break;
           }
         }
-        if (isAdjacentToSymbol) {
-          int numberStart = x;
-          while (numberStart > 0 && is_number(schema[y][numberStart - 1])) {
-            numberStart--;
+        if (is_adjacent_to_symbol) {
+          int number_start = x;
+          while (number_start > 0 && is_number(schema[y][number_start - 1])) {
+            number_start--;
           }
-          if (processedStarts.find(numberStart) == processedStarts.end()) {
-            std::string fullNumber = get_full_number(schema, y, numberStart);
-            sum += std::stoi(fullNumber);
-            processedStarts.insert(numberStart);
+
+          if (processed_starts.find(std::make_pair(y, number_start)) == processed_starts.end()) {
+            std::string full_number = get_full_number(schema, y, number_start);
+            sum += std::stoi(full_number);
+            processed_starts.insert(std::make_pair(y, number_start));
           }
         }
       }
